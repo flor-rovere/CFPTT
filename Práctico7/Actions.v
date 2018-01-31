f55bf04e2cf2817426cf11def8ea131833593492
flor-rovere <flor.rovere@gmail.com>
2018-01-31 16:33:40 -0300

    P7

---
+++ Práctico7/Actions.v
@@ -0,0 +1,170 @@
+(*******************************************************************
+ * Florencia Rovere
+ ******************************************************************)
+
+Load State.
+
+Section Actions.
+
+Inductive Action :=
+   read : vadd -> Action
+ | write : vadd -> value -> Action
+ | chmod : Action.
+
+(* Actions Semantics. *)
+
+Definition va_mapped_to_ma (s: state) (vad: vadd) (mad: madd) : Prop :=
+  exists (o : os) (patoma : padd -> option madd) (mad' : madd) (pg : page) (vatoma : vadd -> option madd),
+    oss s (active_os s) = Some o
+    /\ hypervisor s (active_os s) = Some patoma
+    /\ patoma (curr_page o) = Some mad'
+    /\ memory s mad' = Some pg
+    /\ PT vatoma = page_content pg
+    /\ vatoma vad = Some mad.
+
+Definition Pre_Read (s : state) (vad : vadd) :=
+  os_accesible ctx vad = true
+  /\ aos_activity s = running
+  /\ (exists (mad : madd) (pg : page),
+        va_mapped_to_ma s vad mad
+        /\ memory s mad = Some pg
+        /\ is_RW (page_content pg)).
+
+Definition Pre_Write (s : state) (vad : vadd) (val : value) := Pre_Read s vad.
+
+Definition Pre_Chmod (s : state) :=
+  aos_activity s = waiting
+  /\ exists (o : os),
+       oss s (active_os s) = Some o
+       /\ hcall o = None.
+
+Definition Pre (s : state) (a : Action) : Prop :=
+  match a with
+    read vad => Pre_Read s vad
+  | write vad val => Pre_Write s vad val
+  | chmod =>Pre_Chmod s
+  end.
+
+Definition Post_Read (s : state) (vad : vadd) (s' : state) := s' = s.
+
+Definition aux (mem : system_memory) (mad : madd) (pg : page) :=
+  fun (mad': madd) => if (madd_eq mad mad') then Some pg else mem mad.
+
+Definition Post_Write (s : state) (vad : vadd) (val : value) (s' : state) :=
+  exists (mad : madd),
+    va_mapped_to_ma s vad mad
+    /\ memory s' = aux (memory s) mad (Page (RW (Some val)) (Os (active_os s)))
+    /\ active_os s' = active_os s
+    /\ aos_exec_mode s' = aos_exec_mode s
+    /\ aos_activity s' = aos_activity s
+    /\ oss s' = oss s
+    /\ hypervisor s' = hypervisor s
+    /\ (forall mad' : madd,
+          mad' <> mad
+          /\ memory s mad' = memory s' mad').
+
+Definition Post_Chmod (s s' : state) :=
+  (trusted_os ctx (active_os s) = true
+   /\ aos_exec_mode s = usr
+   /\ aos_exec_mode s' = svc
+   /\ aos_activity s = waiting
+   /\ aos_activity s' = running
+   /\ active_os s' = active_os s
+   /\ oss s' = oss s
+   /\ hypervisor s' = hypervisor s
+   /\ memory s' = memory s)
+  \/ (trusted_os ctx (active_os s) = false
+      /\ aos_exec_mode s = svc
+      /\ aos_exec_mode s' = usr
+      /\ aos_activity s = waiting
+      /\ aos_activity s' = running
+      /\ active_os s' = active_os s
+      /\ oss s' = oss s
+      /\ hypervisor s' = hypervisor s
+      /\ memory s' = memory s).
+
+Definition Post (s : state) (a : Action) (s' : state) : Prop :=
+  match a with
+    read vad => Post_Read s vad s'
+  | write vad val => Post_Write s vad val s'
+  | chmod => Post_Chmod s s'
+  end.
+
+(* One-step execution. *)
+
+Inductive one_step_execution : state -> Action -> state -> Prop :=
+  ose : forall (s s' : state) (a : Action),
+          valid_state s -> Pre s a -> Post s a s' -> one_step_execution s a s'.
+
+
+(* Invariance of valid state. *)
+Ltac manys :=
+  repeat match goal with
+    | [ H : _ /\ _ |- _ ] => destruct H
+    | [ H : exists x, _ |- _ ] => elim H; intros; clear H
+  end.
+
+Lemma preserves_valid_states : forall (s s': state) (a : Action),
+  one_step_execution s a s'
+  -> valid_state_iii s'.
+Proof.
+intros.
+inversion_clear H.
+inversion_clear H0.
+unfold valid_state_iii.
+intro.
+destruct a.
+- inversion H2.
+  apply H.
+  rewrite -> H4 in H0.
+  assumption.
+- inversion H2.
+  manys.
+  rewrite H7.
+  apply H.
+  rewrite <- H6.
+  rewrite <- H8.
+  assumption.
+- inversion H2 ; manys.
+  + assumption.
+  + elim H0 ; intros.
+    * manys.
+      rewrite -> H9 in H14.
+      rewrite H14 in H4.
+      discriminate.
+    * rewrite -> H14 in H8.
+      discriminate.
+Qed.
+
+(* Read isolation. *)
+
+Lemma read_isolation : forall (s s' : state) (vad : vadd),
+  one_step_execution s (read vad) s'
+  -> exists mad : madd,
+       va_mapped_to_ma s vad mad
+       /\ exists pg : page,
+            memory s mad = Some pg
+            /\ page_owned_by pg = Os (active_os s).
+Proof.
+intros.
+inversion_clear H.
+inversion_clear H1.
+manys.
+elim H3.
+intros.
+manys.
+exists x.
+split.
+- assumption.
+- exists x0.
+  split.
+  + assumption.
+  + inversion_clear H0.
+    inversion_clear H3.
+    manys.
+    elim H3 with (pg := x0) (patoma := x7) (pad := curr_page x6) (mad := x8).
+    intros.
+    manys.
+    assumption.
+Qed.
+End Actions.
---
+++ Práctico7/State.v
@@ -0,0 +1,144 @@
+(*******************************************************************
+ * Florencia Rovere
+ ******************************************************************)
+
+Section State.
+
+(* Identifiers for OSs and Hypercalls. *)
+
+Parameter os_ident : Set.
+Parameter os_ident_eq : forall oi1 oi2 : os_ident, {oi1 = oi2} + {oi1 <> oi2}.
+
+Parameter Hyperv_call: Set.
+
+(* Memory and addresses. *)
+
+(* Virtual addresses. *)
+
+Parameter vadd: Set.
+Parameter vadd_eq : forall va1 va2 : vadd, {va1 = va2} + {va1 <> va2}.
+
+(* Machine adresses. *)
+
+Parameter madd :  Set.
+Parameter madd_eq : forall ma1 ma2 : madd, {ma1 = ma2} + {ma1 <> ma2}.
+
+(* Physical adresses. *)
+
+Parameter padd: Set.
+Parameter padd_eq : forall pa1 pa2 : padd, {pa1 = pa2} + {pa1 <> pa2}.
+
+(* Memory values. *)
+
+Parameter value: Set.
+Parameter value_eq:forall val1 val2 : value, {val1 = val2} + {val1 <> val2}.
+
+(* Environment. *)
+
+Record context : Set :=
+  Context
+    { (* A virtual adress is accessible, i.e. it is not reserved
+         for the Hypervisor. *)
+      ctxt_vadd_accessible: vadd -> bool;
+      (* guest OSs  (Trusted/Untrusted) *)
+      ctxt_oss : os_ident -> bool
+    }.
+
+(* Operating systems. *)
+
+Record os : Set :=
+  OS
+    { (* Current page table. *)
+      curr_page : padd;
+      (* Information on whether it has a hypercall pending to be resolved. *)
+      hcall : option Hyperv_call
+    }.
+
+Definition oss_map := os_ident -> option os.
+
+(* Execution mode. *)
+
+Inductive exec_mode := usr | svc.
+
+Inductive os_activity := running | waiting.
+
+(* Memory mappings. *)
+
+Definition hypervisor_map := os_ident -> option (padd -> option madd).
+
+Inductive content :=
+   RW (v : option value)
+ | PT (va_to_ma : vadd -> option madd)
+ | Other.
+
+Definition is_RW (c : content) := exists v : option value, c = RW v.
+
+Inductive page_owner :=
+   Hyp
+ | Os (osi : os_ident)
+ | No_Owner.
+
+Record page : Set :=
+  Page
+    { page_content : content;
+      page_owned_by : page_owner
+    }.
+
+Definition system_memory := madd -> option page.
+
+(* States. *)
+
+Record state : Set :=
+  State
+    {
+      (* Active operating system. *)
+      active_os : os_ident;
+      (* Execution mode. *)
+      aos_exec_mode : exec_mode;
+      (* Processor mode. *)
+      aos_activity : os_activity;
+      (* Information of the guest OSs of the platform. *)
+      oss : oss_map;
+      (* Mapping from physical to machine addresses. *)
+      hypervisor : hypervisor_map;
+      (* Mapping that associates a page to a machine adress. *)
+      memory : system_memory
+    }.
+
+(* Valid states. *)
+
+Parameter ctx : context.
+
+Definition os_accesible := ctxt_vadd_accessible.
+Definition trusted_os := ctxt_oss.
+
+Definition valid_state_iii (s : state) :=
+  (trusted_os ctx (active_os s) = true /\ aos_activity s = running)
+  \/ (aos_activity s = waiting)
+  -> aos_exec_mode s = svc.
+
+Definition valid_state_v (s : state) :=
+  forall (pad : padd) (patoma : padd -> option madd) (mad : madd) (pg : page),
+    hypervisor s (active_os s) = Some patoma
+    /\ patoma pad = Some mad
+    /\ memory s mad = Some pg
+    /\ page_owned_by pg = Os (active_os s)
+    /\ (forall pa pa' : padd,
+          patoma pa = patoma pa' -> pa = pa').
+
+Definition valid_state_vi (s : state) :=
+  forall (vad : vadd) (vatoma : vadd -> option madd) (pg : page) (mad : madd),
+    vatoma vad = Some mad
+    /\ memory s mad = Some pg
+    /\ page_content pg = PT vatoma
+    /\ (os_accesible ctx vad = true
+        -> page_owned_by pg = Os (active_os s))
+    /\ (os_accesible ctx vad = false
+        -> page_owned_by pg = Hyp).
+
+Definition valid_state (s : state) :=
+  valid_state_iii s
+  /\ valid_state_v s
+  /\ valid_state_vi s.
+
+End State.
